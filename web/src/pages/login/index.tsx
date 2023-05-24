4d04c8dcda7772a845991bddc62382a4b47facc5
Jionyh <jiony@live.com>
2023-05-24 11:05:28 -0300

    criado pagina de login

---
+++ web/src/pages/login/index.tsx
@@ -0,0 +1,63 @@
+import Logo from '@/assets/logo.svg'
+import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons'
+import { Button, Input, InputGroup, InputRightElement } from '@chakra-ui/react'
+import Image from 'next/image'
+import React, { useState } from 'react'
+import { api } from '@/libs/axios'
+import { useRouter } from 'next/router'
+const Login = () => {
+  const router = useRouter()
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [show, setShow] = useState(false)
+
+  const handleLogin = async () => {
+    if (email && password) {
+      const res = await api.post('/signin', {
+        email,
+        password,
+      })
+      if (res.data.success) {
+        router.push('/')
+      }
+    }
+  }
+
+  return (
+    <div className="flex h-screen items-center justify-center bg-red-500">
+      <div className="mx-10 flex flex-col items-center justify-center rounded-lg bg-gray-50 px-20 pb-12 pt-5 shadow-2xl">
+        <Image src={Logo} alt="" />
+        <div className="mt-11 flex flex-col gap-8">
+          <Input
+            placeholder="Digite seu email"
+            autoComplete={'false'}
+            shadow="lg"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <InputGroup>
+            <Input
+              placeholder="Digite sua senha"
+              shadow="lg"
+              type={show ? 'text' : 'password'}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <InputRightElement>
+              {show ? (
+                <ViewOffIcon onClick={() => setShow(!show)} />
+              ) : (
+                <ViewIcon onClick={() => setShow(!show)} />
+              )}
+            </InputRightElement>
+          </InputGroup>
+          <Button colorScheme="red" alignSelf="end" onClick={handleLogin}>
+            Entrar
+          </Button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Login
