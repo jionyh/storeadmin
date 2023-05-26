6de2ea9e61f50f67e0a39f0657638f06ff046486
Jionyh <jiony@live.com>
2023-05-26 15:01:59 -0300

    criado pagina home

--- web/src/layout/Layout.tsx
+++ web/src/layout/Layout.tsx
@@ -1,38 +1,25 @@
-import { ReactElement, useContext, useEffect, useState } from 'react'
+import { ReactElement } from 'react'
 import Header from '@/components/Header'
 
-import { Box, Center, Flex, Heading } from '@chakra-ui/react'
+import { Box, Flex, Heading } from '@chakra-ui/react'
 import Head from 'next/head'
 import { ArrowLeftIcon } from '@chakra-ui/icons'
 import { useRouter } from 'next/router'
-import { Context } from '@/contexts/UserContext'
-import dayjs from 'dayjs'
 
 type Props = {
   children: ReactElement
-  title: string
+  title?: string
 }
 
 export const Layout = ({ children, title }: Props) => {
   const router = useRouter()
-  const { state } = useContext(Context)
-  const [message, setMessage] = useState('')
 
-  const checkDayOrNight = () => {
-    const hour = dayjs().hour()
-
-    hour >= 6 && hour < 18 ? setMessage('Bom dia') : setMessage('Boa noite')
-  }
-
-  useEffect(() => {
-    checkDayOrNight()
-  }, [])
   return (
     <>
       <Box className="container max-w-[480px]">
         <Header />
-        <Flex align="center" justifyContent="center" mt={3}>
-          {router.pathname !== '/' && (
+        {router.pathname !== '/' && (
+          <Flex align="center" justifyContent="center" mt={3}>
             <Box
               cursor="pointer"
               transition="all"
@@ -42,22 +29,18 @@ export const Layout = ({ children, title }: Props) => {
             >
               <ArrowLeftIcon boxSize={5} color="gray.600" ml={5} />
             </Box>
-          )}
-          <Heading
-            flex={1}
-            mt="5px"
-            textAlign="center"
-            color="gray.600"
-            size="xl"
-          >
-            {title}
-          </Heading>
-        </Flex>
-        <Center>
-          <Heading size="sm">
-            {message}, {state.user.name}!
-          </Heading>
-        </Center>
+            <Heading
+              flex={1}
+              mt="5px"
+              textAlign="center"
+              color="gray.600"
+              size="xl"
+            >
+              {title}
+            </Heading>
+          </Flex>
+        )}
+
         <hr className="m-5" />
         <Head>
           <title>{title}</title>
--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,9 +1,171 @@
+import { DoughnutChart } from '@/components/Charts/doughnut'
+import { Context } from '@/contexts/UserContext'
 import { Layout } from '@/layout/Layout'
+import {
+  Alert,
+  AlertIcon,
+  Box,
+  Card,
+  CardBody,
+  CardHeader,
+  Center,
+  Divider,
+  Flex,
+  Select,
+  SimpleGrid,
+  Tag,
+  TagLeftIcon,
+  TagRightIcon,
+  Text,
+} from '@chakra-ui/react'
+import { TbMoneybag } from 'react-icons/tb'
+import dayjs from 'dayjs'
+import { useContext, useEffect, useState } from 'react'
+import { BiPurchaseTag } from 'react-icons/bi'
 
 export default function Home() {
+  const { state } = useContext(Context)
+
+  const [message, setMessage] = useState('')
+
+  const checkDayOrNight = () => {
+    const hour = dayjs().hour()
+
+    if (hour >= 6 && hour < 12) {
+      setMessage('Bom dia')
+    } else if (hour >= 12 && hour < 18) {
+      setMessage('Boa tarde')
+    } else {
+      setMessage('Boa noite')
+    }
+
+    // hour >= 6 && hour < 12 ? setMessage('Bom dia') : setMessage('Boa noite')
+  }
+
+  useEffect(() => {
+    checkDayOrNight()
+  }, [])
   return (
-    <Layout title="Home">
-      <h1>Home</h1>
+    <Layout>
+      <Box p="2px">
+        <Alert status="warning" variant="left-accent" mb="10px">
+          <AlertIcon />
+          <Text align="right" fontSize="md" lineHeight={6} fontStyle="normal">
+            {message}, {state.user.name}
+          </Text>
+        </Alert>
+        <SimpleGrid columns={2} spacing={5}>
+          <Card variant="elevated" background="blue.100" p="10px">
+            <CardHeader p="5px">
+              <Tag
+                size="md"
+                borderRadius="lg"
+                variant="solid"
+                colorScheme="blue"
+              >
+                <TagLeftIcon as={TbMoneybag} m="0px" />
+                <Text
+                  pl="5px"
+                  align="left"
+                  fontSize="md"
+                  lineHeight={6}
+                  fontStyle="normal"
+                  fontWeight="bold"
+                >
+                  Vendas
+                </Text>
+              </Tag>
+              <Divider py="2px" />
+            </CardHeader>
+            <CardBody p="5px">
+              <Text>
+                Dia - <strong>€ 0,00</strong>
+              </Text>
+              <Text>
+                7 Dias - <strong>€ 8,00</strong>
+              </Text>
+              <Text>
+                30 Dias - <strong>€ 63,00</strong>
+              </Text>
+            </CardBody>
+          </Card>
+          <Card variant="elevated" background="blue.100" p="10px">
+            <CardHeader p="5px">
+              <Tag
+                size="md"
+                borderRadius="lg"
+                variant="solid"
+                colorScheme="blue"
+              >
+                <TagLeftIcon as={BiPurchaseTag} m="0px" />
+                <Text
+                  pl="5px"
+                  align="left"
+                  fontSize="md"
+                  lineHeight={6}
+                  fontStyle="normal"
+                  fontWeight="bold"
+                >
+                  Compras
+                </Text>
+              </Tag>
+              <Divider py="2px" />
+            </CardHeader>
+            <CardBody p="5px">
+              <Text>
+                Dia - <strong>€ 0,00</strong>
+              </Text>
+              <Text>
+                7 Dias - <strong>€ 8,00</strong>
+              </Text>
+              <Text>
+                30 Dias - <strong>€ 63,00</strong>
+              </Text>
+            </CardBody>
+          </Card>
+        </SimpleGrid>
+        <Card>
+          <CardHeader
+            background="green.100"
+            w="100%"
+            p="10px"
+            mt="10px"
+            roundedTop="lg"
+          >
+            <Flex alignItems="center" justifyContent="space-evenly" gap="5px">
+              <Text
+                align="right"
+                color="gray.900"
+                fontSize="md"
+                lineHeight={6}
+                fontStyle="normal"
+                fontWeight="bold"
+              >
+                Detalhes de
+              </Text>
+              <Select width="auto" variant="filled" color="green.800">
+                <option>Compras</option>
+                <option>Vendas</option>
+              </Select>
+              <Select width="auto" variant="filled" color="green.800">
+                <option>Dia</option>
+                <option defaultChecked>Ultimos 7 dias</option>
+                <option>Ultimos 30 dias</option>
+              </Select>
+            </Flex>
+          </CardHeader>
+          <CardBody border="1px" borderColor="blackAlpha.300">
+            <Center flexDirection="column" gap="10px">
+              <Select width="200px" variant="filled" color="gray.800">
+                <option>Categoria</option>
+                <option defaultChecked>Ultimos 7 dias</option>
+                <option>Ultimos 30 dias</option>
+              </Select>
+              <DoughnutChart />
+            </Center>
+          </CardBody>
+        </Card>
+      </Box>
     </Layout>
   )
 }
