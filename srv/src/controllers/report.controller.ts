9cfc3a40aa8181e7fc0335497dc4d1914dc6b8cc
jionyh <jiony@live.com>
2023-12-27 15:49:10 -0300

    refatorado controller: locale pt-br

--- srv/src/controllers/report.controller.ts
+++ srv/src/controllers/report.controller.ts
@@ -2,16 +2,18 @@ import { Request, Response } from "express";
 import { defaultDate } from "../utils/dateUtils";
 import * as reportService from "../services/report.service";
 import dayjs from "dayjs";
+import "dayjs/locale/pt-br";
+
+dayjs.locale("pt-br");
 
 export const report = {
   cashflow: async (req: Request, res: Response) => {
     const date = defaultDate();
 
     const inflow = await reportService.inflow(req.tenant_id, date);
-
     const outflow = await reportService.outflow(req.tenant_id, date);
 
-    if (!inflow || !outflow) return res.json({ error: "inflow not found" });
+    if (!inflow || !outflow) return res.json({ error: "cashflow not found" });
 
     const formatReturn = Array.from({ length: 6 }, (_, i) => {
       const start = dayjs(date).subtract(i, "months").startOf("month").toDate();
